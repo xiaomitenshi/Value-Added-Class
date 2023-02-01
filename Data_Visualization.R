d06e211afd304aef906936a662517b4c649ac1ea
Gokul <93534067+xiaomitenshi@users.noreply.github.com>
2023-02-01 11:24:06 +0530

    Create Data_Visualization.R

---
+++ Data_Visualization.R
@@ -0,0 +1,23 @@
+count<-table(mtcar$gear)
+view(count)
+plot(count)
+
+barplot(count)
+barplot(count, horiz=TRUE)
+barplot(count, main="Sample bar plot",
+        xlab = "Improvement",
+        ylab = "Frequency",
+        legend = rownames(count),
+        col=c("red","yellow","green"))
+pie(count)
+salary<-read.csv("Salary_Data.csv")
+library(ggplot2)
+
+ggplot(salary,aes(x=Experience, y=Salary))+
+  geom_point()
+
+ggplot(salary,aes(x=Experience, y=Salary))+
+  geom_line()
+
+ggplot(salary,aes(x=Experience, y=Salary))+
+  geom_boxplot()
